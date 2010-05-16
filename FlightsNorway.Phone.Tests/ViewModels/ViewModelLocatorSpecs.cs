6ac282f456f60e4aef93234a82a27c34b4fdb696
Jonas Folles <jonas@follesoe.no>
2010-05-17 00:14:23 +0200

    Made ViewModels returned from ViewModelLocator singletons.

---
+++ FlightsNorway.Phone.Tests/ViewModels/ViewModelLocatorSpecs.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlightsNorway.Phone.Tests.ViewModels
+{
+    [TestClass]
+    public class ViewModelLocatorSpecs
+    {
+        [TestMethod, Tag(Tags.ViewModel)]
+        public void Airports_ViewModel_is_singleton()
+        {
+            var airports1 = locator.AirportsViewModel;
+            var airports2 = locator.AirportsViewModel;
+            Assert.AreSame(airports1, airports2);
+        }
+
+        [TestMethod, Tag(Tags.ViewModel)]
+        public void Flights_ViewModel_is_singleton()
+        {
+            var flights1 = locator.FlightsViewModel;
+            var flights2 = locator.FlightsViewModel;
+            Assert.AreSame(flights1, flights2);
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            locator = new ViewModelLocator();
+        }
+
+        private ViewModelLocator locator;
+    }
+}
--- FlightsNorway.Phone/ViewModelLocator.cs
+++ FlightsNorway.Phone/ViewModelLocator.cs
@@ -4,9 +4,17 @@ namespace FlightsNorway.Phone
 {
     public class ViewModelLocator
     {
+        private AirportsViewModel _airportsViewModel;
+        private FlightsViewModel _flightsViewModel;
+
         public AirportsViewModel AirportsViewModel
         {
-            get { return new AirportsViewModel(); }
+            get { return _airportsViewModel ?? (_airportsViewModel = new AirportsViewModel()); }
+        }
+
+        public FlightsViewModel FlightsViewModel
+        {
+            get { return _flightsViewModel ?? (_flightsViewModel = new FlightsViewModel()); }
         }
     }
 }
