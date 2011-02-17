9fd4cfaed7c92700644bcc88ea3617f064163f66
Jonas Folleso <jonas@follesoe.no>
2011-02-17 16:26:09 +0100

    Moved event to seperate file.

---
+++ FlightsNorway.Phone/Services/EventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FlightsNorway.Services
+{
+    public class EventArgs<T> : EventArgs
+    {
+        public T Content { get; private set; }
+
+        public EventArgs(T content)
+        {
+            Content = content;
+        }
+    }
+}
--- FlightsNorway.Phone/Services/IGetCurrentLocation.cs
+++ FlightsNorway.Phone/Services/IGetCurrentLocation.cs
@@ -3,16 +3,6 @@ using FlightsNorway.Model;
 
 namespace FlightsNorway.Services
 {
-    public class EventArgs<T> : EventArgs
-    {
-        public T Content { get; private set; }
-
-        public EventArgs(T content)
-        {
-            Content = content;
-        }
-    }
-
     public interface IGetCurrentLocation
     {
         event EventHandler<EventArgs<Location>> PositionAvailable;
