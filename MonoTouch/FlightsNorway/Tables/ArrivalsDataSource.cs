1d4b7342a17f4b70a7004aa8e0be0e138b2940ae
Jonas Folleso <jonas@follesoe.no>
2011-03-16 17:06:52 +0100

    Migrating away from generic base class for simplicity.

--- MonoTouch/FlightsNorway/Tables/ArrivalsDataSource.cs
+++ MonoTouch/FlightsNorway/Tables/ArrivalsDataSource.cs
@@ -8,29 +8,40 @@ using System.Collections.ObjectModel;
 
 namespace FlightsNorway
 {
-	public class ArrivalsDataSource : ViewModelDataSource<FlightsViewModel, Flight>
+	public class ArrivalsDataSource : UITableViewDataSource
 	{
+		public UITableView TableView { get; set; }
+		
+		private readonly FlightsViewModel _viewModel;
+		
 		private NSString _cellID = new NSString("ArrivalCell");
-		
-		public override NSString CellID { get { return _cellID; } }
-				
-		public override ObservableCollection<Flight> List { get { return ViewModel.Arrivals; } }				
-		
-		public ArrivalsDataSource(FlightsViewModel viewModel) : base(viewModel)
+						
+		public ArrivalsDataSource(FlightsViewModel viewModel)
 		{
+			_viewModel = viewModel;
+			
+			_viewModel.Arrivals.CollectionChanged += (o, e) => {
+				if(TableView != null)
+					TableView.ReloadData();
+			};
 		}
+		
+		public override int RowsInSection (UITableView tableView, int section)
+		{
+			return _viewModel.Arrivals.Count;
+		}	
 		
 		public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
 		{
-			var cell = tableView.DequeueReusableCell(CellID);
+			var cell = tableView.DequeueReusableCell(_cellID);
             if (cell == null)
             {					
-                cell = new UITableViewCell(UITableViewCellStyle.Subtitle, CellID);
+                cell = new UITableViewCell(UITableViewCellStyle.Subtitle, _cellID);
 				cell.TextLabel.Font = UIFont.FromName("Georgia", 16f);				
 				cell.DetailTextLabel.Font = UIFont.FromName("Georgia", 12f);
             }
         				
-			var flight = List[indexPath.Row];
+			var flight = _viewModel.Arrivals[indexPath.Row];
             cell.TextLabel.Text = flight.Line1();				
 			cell.DetailTextLabel.Text = flight.Line2();
             return cell;
