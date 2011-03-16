97226cda860f44af74ada275967e046f7cc2d238
Jonas Folleso <jonas@follesoe.no>
2011-03-16 18:11:49 +0100

    Impemented dispatcher on MonoTouch.

--- MonoTouch/FlightsNorway/ContainerConfiguration.cs
+++ MonoTouch/FlightsNorway/ContainerConfiguration.cs
@@ -14,18 +14,31 @@ namespace FlightsNorway
 		{
 			var container = TinyIoC.TinyIoCContainer.Current;
 			
+			container.Register<IDispatchOnUIThread>(new Dispatcher(owner));
 			container.Register<AirportsViewModel>().AsSingleton();
 			container.Register<FlightsViewModel>().AsSingleton();
 			
 			container.Register<IStoreObjects, MonoObjectStore>().AsSingleton();
 			container.Register<IGetFlights, FlightsService>().AsSingleton();
-			container.Register<IGetAirports, AirportNamesService>().AsSingleton();
-			
+			container.Register<IGetAirports, AirportNamesService>().AsSingleton();			
 					
-			container.Register<IGeolocation>(
-                       new PresetLocationService(63.433281, 10.419294, action => owner.InvokeOnMainThread(new NSAction(action))));												
-			
+			container.Register<IGeolocation>(new PresetLocationService(63.433281, 10.419294, container.Resolve<IDispatchOnUIThread>()));			
 			container.Register(new NearestAirportService(container.Resolve<IGeolocation>(), container.Resolve<ITinyMessengerHub>()));			
 		}			
+	}
+	
+	public class Dispatcher : IDispatchOnUIThread
+	{
+		private readonly NSObject _owner;
+		
+		public Dispatcher(NSObject owner)
+		{
+			_owner = owner;
+		}
+		
+		public void Invoke (Action action)
+		{
+			_owner.BeginInvokeOnMainThread(new NSAction(action));
+		}		
 	}
 }
