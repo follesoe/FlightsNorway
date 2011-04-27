dd885672cf63e534a5fce25d38b83855c1fd4e2e
Jonas Folleso <jonas@follesoe.no>
2011-04-27 23:35:08 +0200

    Trying to get it to work on MT4

--- MonoTouch/FlightsNorway/Main.cs
+++ MonoTouch/FlightsNorway/Main.cs
@@ -1,22 +1,12 @@
-using System;
-using System.Linq;
-using System.Collections.Generic;
-
 using MonoTouch.UIKit;
-using MonoTouch.Foundation;
-
-using FlightsNorway.Lib.Services;
-using FlightsNorway.Lib.DataServices;
-using FlightsNorway.Lib.ViewModels;
 
 namespace FlightsNorway
 {
 	public class Application
 	{
 		static void Main (string[] args)
-		{	
+		{
 			UIApplication.Main(args);
 		}
 	}
 }
-
